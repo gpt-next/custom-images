ee74535146f26292f2a60e1991fd9bfad3f1416c
Chris Felix <chrisfel@ca.ibm.com>
2023-11-08 18:36:43 -0600

    Update start.sh

--- github-runner/start.sh
+++ github-runner/start.sh
@@ -3,8 +3,8 @@
 REPOSITORY=$REPO
 ACCESS_TOKEN=$TOKEN
 
-echo "REPO ${REPOSITORY}"
-echo "ACCESS_TOKEN ${ACCESS_TOKEN}"
+#echo "REPO ${REPOSITORY}"
+#echo "ACCESS_TOKEN ${ACCESS_TOKEN}"
 
 REG_TOKEN=$(curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $ACCESS_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/orgs/$REPOSITORY/actions/runners/registration-token | jq .token --raw-output)
 cd /home/build/actions-runner
